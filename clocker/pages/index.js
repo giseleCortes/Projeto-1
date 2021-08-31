feb9c01efbc0ee9556d022ddf2e08574b6f5f613
Gisele Cortes <gisele.cortes@outlook.com>
2021-08-31 05:24:18 -0300

    temporaria solução para a questão do logo

--- clocker/pages/index.js
+++ clocker/pages/index.js
@@ -2,9 +2,11 @@ import { Logo } from './../components/Logo'
 
 
 export default function Home() {
-  return( // quando adiciono <Logo /> na div a baixo, o codigo entra em colapso // verificar porque depois 
+  return( // quando adiciono <Logo /> na div a baixo, o codigo entra em colapso // 
+
     <div>
-      Clocker io 
+      <Logo />
     </div>
   )
 }
+// consegui uma solução mais sujou o codigo, verificar depois //
