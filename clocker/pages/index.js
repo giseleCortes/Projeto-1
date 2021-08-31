58cbb99f99f1b59cc08b809e5261f3b7a90d8d64
Gisele Cortes <gisele.cortes@outlook.com>
2021-08-31 05:51:04 -0300

    editando pag sing up
    comentários importantes

--- clocker/pages/index.js
+++ clocker/pages/index.js
@@ -1,12 +1,43 @@
+
+import {
+  Container,
+  Box,
+  Input,
+  Button,
+  Text,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+} from '@chakra-ui/react'
 import { Logo } from './../components/Logo'
 
 
 export default function Home() {
-  return( // quando adiciono <Logo /> na div a baixo, o codigo entra em colapso // 
+  return ( // quando adiciono <Logo /> na div a baixo o codigo entra em colapso // 
 
-    <div>
+    //padding no chakra é por escala por isso  p={4} verificar na documentação//
+    // alias verificar tudo na verificação do chakra sobre a pagina de singup //
+    //<FormHelperText>We'll never share your email.</FormHelperText> verificar sobre validação depois  //
+    <Container p={4} centerContent>
       <Logo />
-    </div>
+      <Box p={4} mt={8}>
+        <Text>Crie sua agenda compartilhada</Text>
+      </Box>
+
+      <FormControl id="email" isRequired>
+        <FormLabel>Email</FormLabel>
+        <Input type="email" />         
+      </FormControl>
+
+      <FormControl id="password" isRequired>
+        <FormLabel>Senha</FormLabel>
+        <Input type="password" />         
+      </FormControl>
+
+
+
+    </Container>
+
   )
 }
-// consegui uma solução mais sujou o codigo, verificar depois //
+// consegui uma solução mais sujou o codigo verificar depois //
